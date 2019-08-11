ef8130420c4ab965348402ee00b08afc9f8c9e63
Candy <candy.espulgar@gmail.com>
2019-08-11 19:20:29 +0900

    GS Mat Animations

--- Assets/_Scripts/Soldiers/BackupMatAnimHandler.cs
+++ Assets/_Scripts/Soldiers/BackupMatAnimHandler.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class BackupMatAnimHandler : MonoBehaviour {
 
 	public const string OPEN_STATE = "ShouldOpen";
+    public const string IDLE_STATE = "isIdle";
 
-	private Animator anim;
+    private Animator anim;
 	private SoldierBackup[] backups;
 	private bool isOpened;
 	public bool HasOpened {
@@ -19,16 +20,25 @@ public class BackupMatAnimHandler : MonoBehaviour {
 		isOpened = false;
 	}
 
+    public bool Idle(bool val) {
+        anim.SetBool(IDLE_STATE, val);
+        return true;
+    }
+
 	public bool Open(bool val) {
-		if (!val) {
+		if (!val) { // If closed
 			foreach (SoldierBackup b in backups) {
 				if (b.IsHidden)
 					return false;       // Failed Hide. Some backupsoldiers still not hidden.
 			}
 			foreach (SoldierBackup b in backups) {
 				b.SetHidden(true);
-			}
-		}
+            }
+            this.Idle(true);
+        }
+        else { // If opened, set idle false
+            this.Idle(false);
+        }
 		anim.SetBool(OPEN_STATE, val);
 		isOpened = val;
 		return true;
