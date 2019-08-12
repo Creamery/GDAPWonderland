0219ff11e2f3095829a0457900f89ae6eda8dff0
Candy <candy.espulgar@gmail.com>
2019-08-12 19:20:47 +0900

    History Script Setup

---
+++ Assets/_Scripts/History/SoldierHistoryManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierHistoryManager : MonoBehaviour {
+
+    [SerializeField] protected SoldierHistoryModels soldierHistoryModels;
+}
---
+++ Assets/_Scripts/History/SoldierHistoryModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierHistoryModels : MonoBehaviour {
+
+    [SerializeField] protected TextMeshTeamLeadObjectMarker textTeamLead;
+    [SerializeField] protected TextMeshTeamAssistObjectMarker textTeamAssist;
+
+    [SerializeField] protected TextMeshTargetLeadObjectMarker textTargetLead;
+    [SerializeField] protected TextMeshTargetAssistObjectMarker textTargetAssist;
+
+
+
+
+
+}
