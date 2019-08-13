697ba8a9987a9dc9ae26c3c4b90540d1edc64b4c
Candy <candy.espulgar@gmail.com>
2019-08-14 01:42:13 +0900

    GS Major Edit

--- Assets/_Scripts/Soldiers/SoldierBackupFloatingCard.cs
+++ Assets/_Scripts/Soldiers/SoldierBackupFloatingCard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SoldierBackupFloatingCard : MonoBehaviour {
     [SerializeField] MeshRenderer cardRenderer;
+    [SerializeField] MeshRenderer[] childRenderers;
     protected bool isEnabled = true;
 
     public void Refresh() {
@@ -21,11 +22,19 @@ public class SoldierBackupFloatingCard : MonoBehaviour {
     */
     public void Show() {
         this.GetCardRenderer().enabled = true;
+        // Handle children
+        foreach (var component in this.GetChildRenderers())
+            ((MeshRenderer)component).enabled = true;
         isEnabled = true;
     }
 
     public void Hide() {
         this.GetCardRenderer().enabled = false;
+
+        // Handle children
+        foreach (var component in this.GetChildRenderers())
+            ((MeshRenderer)component).enabled = false;
+
         isEnabled = false;
     }
 
@@ -34,5 +43,12 @@ public class SoldierBackupFloatingCard : MonoBehaviour {
             this.cardRenderer = GetComponent<MeshRenderer>();
         }
         return this.cardRenderer;
+    }
+
+    public MeshRenderer[] GetChildRenderers() {
+        if (this.childRenderers == null || this.childRenderers.Length == 0) {
+            this.childRenderers = GetComponentsInChildren<MeshRenderer>();
+        }
+        return this.childRenderers;
     }
 }
